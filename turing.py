2de4d5fdf6f83b030e7b4db6c8e85ee1edd120f4
Hec0nP0int <HECTORMIEL@GMAIL.COM>
2025-10-25 11:34:09 -0600

    Add files via upload
    Intro to Python for Computer Science and Data Science :Learning to program with AI, Big Data and the cloud
    Paul Deitel, Harvey Deitel Book ISBN-13: 978-0-13-540467-6
    Exercise 3.6 Turing Test

---
+++ turing.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+3.6 Turing Test 
+
+@author: hec
+"""
+
+input('What is wrong with you?') #initial response /Ignore
+
+response1 = (input('Have you had this problem before? ')) # Process Response
+if response1 == 'yes':
+        print('Well you have it again')
+else:        
+    print('Well you have it now')
+        
+        
+############################################
+# Hector Practice Exercise Code            #
+#                                          #
+#                                          # 
+#                                          #
+############################################
